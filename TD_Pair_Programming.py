248670e0b804eae7d3c5d94b9acc0152f2c23c16
maxim <maxime.richaudeau1@gmail.com>
2021-09-20 12:48:13 +0200

    Guillaume commit

--- TD_Pair_Programming.py
+++ TD_Pair_Programming.py
@@ -1,2 +1,10 @@
-#-*- coding:Utf-8 -*-
+# -*- coding:Utf-8 -*-
 """ Fichier rendu du TD PairProgramming Hannotte & Richaudeau """
+
+
+def hello_world():
+    print('Hello world')
+
+
+if __name__ == '__main__':
+    hello_world()
